37357f7b4379861bc8b33db19fcd6c3e52d79d37
Shlomi Nugarker <shlomin1231@gmail.com>
2023-01-08 22:14:29 +0200

    deleted highlight.js, installed react-ace

--- src/pages/CodeBlockPage.tsx
+++ src/pages/CodeBlockPage.tsx
@@ -1,12 +1,18 @@
 import { useCallback, useEffect, useState } from 'react'
-import hljs from 'highlight.js'
-import 'highlight.js/styles/github.css'
 import { useParams } from 'react-router-dom'
+
 import { codeBlockService } from '../services/codeBlockService'
 import { ICodeBlock } from '../interfaces/ICodeBlock'
 
+import AceEditor from 'react-ace'
+import 'ace-builds/src-noconflict/mode-java'
+import 'ace-builds/src-noconflict/theme-github'
+import 'ace-builds/src-noconflict/theme-terminal'
+import 'ace-builds/src-noconflict/theme-monokai'
+import 'ace-builds/src-noconflict/ext-language_tools'
+
 export default function CodeBlockPage() {
-  let { id } = useParams()
+  const { id } = useParams()
   const [codeBlock, setCodeBlock] = useState<ICodeBlock | null>(null)
 
   const loadCode = useCallback(async () => {
@@ -14,24 +20,45 @@ export default function CodeBlockPage() {
     try {
       const code = await codeBlockService.getById(id)
       setCodeBlock(code)
-      hljs.highlightAll()
     } catch (err) {
-      alert('cant find code block')
       console.log(err)
+      alert('cant find code block')
     }
   }, [id])
+
+  const handleChange = (newValue: string) => {
+    setCodeBlock((prevVal) => ({ ...prevVal, code: newValue } as ICodeBlock))
+  }
 
   useEffect(() => {
     loadCode()
   }, [loadCode])
 
-  if (!codeBlock) return <p>Loading...</p>
+  if (!codeBlock) return <p className="code-block-page">Loading...</p>
   return (
-    <div className="code-page">
-      <h1>{codeBlock.title}</h1>
-      <pre>
-        <code className="language-js">{codeBlock.code}</code>
-      </pre>
+    <div className="code-block-page">
+      <div className="code-block-edit">
+        <h1 className="title">{codeBlock.title}</h1>
+        <AceEditor
+          placeholder=""
+          mode="javascript"
+          theme="monokai"
+          fontSize={16}
+          showPrintMargin={true}
+          showGutter={true}
+          onChange={handleChange}
+          name="text-area-block-code"
+          value={codeBlock.code}
+          editorProps={{ $blockScrolling: true }}
+          setOptions={{
+            enableBasicAutocompletion: true,
+            enableLiveAutocompletion: true,
+            enableSnippets: true,
+            showLineNumbers: true,
+            tabSize: 2,
+          }}
+        />
+      </div>
     </div>
   )
 }

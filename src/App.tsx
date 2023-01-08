0ed595880ccf63a05304ff084d574a5185e0d399
Shlomi Nugarker <shlomin1231@gmail.com>
2023-01-08 14:53:01 +0200

    installed highlight.js, added basic example

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
 import './assets/scss/global.scss'
 
-function App() {
+import { useEffect } from 'react'
+import hljs from 'highlight.js'
+
+import 'highlight.js/styles/github.css'
+
+export default function App() {
+  const markdown = `
+    const variable = 'hello';
+
+    function getProfile(id: string): {
+      name: string; address: string, photo: string
+    } {
+      return {
+        name: 'ben', address: "ben's house", photo: "/ben.png"
+      };
+    }
+ 
+`
+
+  useEffect(() => {
+    hljs.highlightAll()
+  })
+
   return (
     <div className="App">
-      <h1>App</h1>
+      <h1>title</h1>
+      <pre>
+        <code className="language-js">{markdown}</code>
+      </pre>
     </div>
   )
 }
-
-export default App

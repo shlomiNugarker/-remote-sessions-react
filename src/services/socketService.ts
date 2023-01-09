a2f220b02ddbeef63fe5ab09e77b0a3fd4198709
Shlomi Nugarker <shlomin1231@gmail.com>
2023-01-09 11:47:16 +0200

    socket service - client

---
+++ src/services/socketService.ts
@@ -0,0 +1,31 @@
+import { DefaultEventsMap } from '@socket.io/component-emitter'
+import io, { Socket } from 'socket.io-client'
+
+const baseUrl = process.env.NODE_ENV === 'production' ? '' : '//localhost:3030'
+export const socketService = createSocketService()
+
+socketService.setup()
+
+function createSocketService() {
+  let socket: Socket<DefaultEventsMap, DefaultEventsMap> | null = null
+  const socketService = {
+    async setup() {
+      socket = io(baseUrl)
+    },
+    on(eventName: string, cb: any) {
+      socket && socket.on(eventName, cb)
+    },
+    off(eventName: string, cb = null) {
+      if (!socket) return
+      if (!cb) socket.removeAllListeners(eventName)
+      else socket.off(eventName, cb)
+    },
+    emit(eventName: string, data: any) {
+      socket && socket.emit(eventName, data)
+    },
+    terminate() {
+      socket = null
+    },
+  }
+  return socketService
+}

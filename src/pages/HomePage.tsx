9eba35d057fd8b398b10b345f074dc3da5e7f8a0
Shlomi Nugarker <shlomin1231@gmail.com>
2023-01-16 13:22:42 +0200

    opt to logout

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,15 @@
 import { useNavigate } from 'react-router-dom'
 
 import loadingGif from '../assets/imgs/loading.gif'
+import { authService } from '../services/authService'
+import { IUser } from '../interfaces/IUser'
 
-export default function Home({
-  codeBlocksIds,
-}: {
+type Props = {
   codeBlocksIds: { _id: string; title: string }[] | null
-}) {
+  loggedUser: IUser | null
+}
+
+export default function Home({ codeBlocksIds, loggedUser }: Props) {
   const navigate = useNavigate()
   if (!codeBlocksIds)
     return (
@@ -18,13 +21,28 @@ export default function Home({
   if (!codeBlocksIds.length)
     return <div className="home-page">No block codes yet... 🙂</div>
 
+  const logout = () => {
+    try {
+      authService.logout()
+      navigate('/sign-in')
+    } catch (err) {
+      console.log(err)
+      alert("couldn't logout...")
+    }
+  }
+
   return (
     <section className="home-page">
+      <div className="logout-btn">
+        <p>Hello, {loggedUser?.fullName}</p>
+        <button onClick={logout}>Logout</button>
+      </div>
       <h1>Choose code block:</h1>
 
       <div className="code-blocks">
         {codeBlocksIds.map((codeBlock) => (
           <button
+            className="code-block-btn"
             key={codeBlock._id}
             onClick={() => navigate(`/${codeBlock._id}`)}
           >

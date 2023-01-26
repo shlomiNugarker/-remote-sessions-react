efd25389f6c0af6ee4e5ffbd5a5ad6d7d77ea3c3
Shlomi Nugarker <shlomin1231@gmail.com>
2023-01-26 11:41:58 +0200

    router as browserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import { HashRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+// import { HashRouter as Router } from 'react-router-dom'
 
-// react-ace (editor):
+// // react-ace (editor):
 import 'ace-builds/webpack-resolver'
 import 'ace-builds/src-noconflict/mode-javascript'
 import 'ace-builds/src-noconflict/mode-typescript'
